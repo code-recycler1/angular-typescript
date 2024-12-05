0bb88e3d859629917a6773cbe2cb5589553b3f27
r0302772 <dennis.nijs@hotmail.com>
2024-12-05 13:00:39 +0100

    added comments to CSS article component

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article',
+  standalone: true,
   imports: [],
   templateUrl: './article.component.html',
-  styleUrl: './article.component.css'
+  styleUrls: ['./article.component.css']
 })
 export class ArticleComponent {
 
