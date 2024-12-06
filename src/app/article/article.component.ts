cc959459b354f922b41a61d043a7985b2fec43fe
r0302772 <dennis.nijs@hotmail.com>
2024-12-06 18:13:24 +0100

    refactoring Article component to use interface

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,9 +1,11 @@
-// Import necessary Angular core module
-import { Component } from '@angular/core';
+// Import necessary Angular core modules for creating components and lifecycle hooks
+import { Component, Input, OnInit } from '@angular/core';
+// Import the Article interface or class to type-check the input property
+import { Article } from '../article';
 
-// Decorator to define the metadata for the ArticleComponent
+// Decorator to define metadata for the ArticleComponent
 @Component({
-  // Selector specifies the custom HTML tag to use this component
+  // Selector specifies the custom HTML tag that will represent this component
   selector: 'app-article',
 
   // Marks this component as standalone, meaning it doesn't need to be declared in an NgModule
@@ -19,6 +21,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 // The main class for the ArticleComponent, which contains logic and properties for the component
-export class ArticleComponent {
-  // Currently, this component has no properties or methods defined.
+export class ArticleComponent implements OnInit {
+  // Input property to receive an `Article` object from a parent component
+  // The `!` non-null assertion operator indicates that this property will always be initialized by the parent
+  @Input() article!: Article;
+
+  // Constructor for the component, used for dependency injection (empty in this case)
+  constructor() { }
+
+  // Angular lifecycle hook called once after the component's data-bound properties are initialized
+  ngOnInit(): void {
+    // Logic to run when the component is initialized (currently empty)
+  }
 }
